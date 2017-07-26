4869c2e72882b6dfe6b903463caf8256b3ea9d70
Sandro Salari <sandro.salari@gmail.com>
2017-07-26 17:41:29 +0200

    Refactored layer dimension retriver

--- geowebcache/wmts/src/main/java/org/geowebcache/service/wmts/WMTSUtils.java
+++ geowebcache/wmts/src/main/java/org/geowebcache/service/wmts/WMTSUtils.java
@@ -1,9 +1,11 @@
 package org.geowebcache.service.wmts;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.geowebcache.filter.parameters.ParameterFilter;
 import org.geowebcache.layer.TileLayer;
 import org.geowebcache.mime.MimeType;
 
@@ -15,4 +17,21 @@ public final class WMTSUtils {
     protected static List<String> getLayerFormats(TileLayer layer) throws IOException {
         return layer.getMimeTypes().stream().map(MimeType::getFormat).collect(Collectors.toList());
     }
+
+    public static List<String> getInfoFormats(TileLayer layer) {
+        return layer.getInfoMimeTypes().stream().map(MimeType::getFormat)
+                .collect(Collectors.toList());
+    }
+
+    public static List<ParameterFilter> getLayerDimensions(List<ParameterFilter> filters) {
+        List<ParameterFilter> dimensions = new ArrayList<ParameterFilter>(0);
+        if (filters != null) {
+            dimensions = filters.stream()
+                    .filter(filter -> !"STYLES".equalsIgnoreCase(filter.getKey())
+                            && filter.getLegalValues() != null)
+                    .collect(Collectors.toList());
+        }
+        return dimensions;
+    }
+
 }
